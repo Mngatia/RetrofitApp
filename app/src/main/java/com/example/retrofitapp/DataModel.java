6be01205c7547fb8364263c2e0e736610365697e
Mngatia <nyawirahngatia@gmail.com>
2021-01-08 23:56:13 +0300

    Retrofit - GET simple JSON

--- app/src/main/java/com/example/retrofitapp/DataModel.java
+++ app/src/main/java/com/example/retrofitapp/DataModel.java
@@ -8,9 +8,28 @@ public class DataModel {
     private int userId;
     private  int id;
     private String title;
+    private boolean completed;
+
+    // @SerializedName("completed")
+    //private boolean status;
 
 
-    @SerializedName("completed")
-    private boolean status;
+    //getters
 
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
 }
