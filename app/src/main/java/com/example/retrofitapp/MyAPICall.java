43236f3a2b0f76024304fa5c0e928e1d897042e1
Mngatia <nyawirahngatia@gmail.com>
2021-01-09 19:38:04 +0300

    Retrofit - GET simple JSON

--- app/src/main/java/com/example/retrofitapp/MyAPICall.java
+++ app/src/main/java/com/example/retrofitapp/MyAPICall.java
@@ -4,9 +4,9 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface MyAPICall {
-    //  https://run.mocky.io/v3/    90fea295-8b4d-4e7c-be1e-f0788188102f
+    //  https://run.mocky.io/   v3/90fea295-8b4d-4e7c-be1e-f0788188102f
 
-    @GET("90fea295-8b4d-4e7c-be1e-f0788188102f")
+    @GET("v3/90fea295-8b4d-4e7c-be1e-f0788188102f")
     Call<DataModel> getData();
 
 
